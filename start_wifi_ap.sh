038726681e67f3d3c35288f27bbd9db91250cba3
Daniel Reinoso <danielreinoso1807@gmail.com>
2017-12-05 21:12:33 +0000

    change ap script

--- start_wifi_ap.sh
+++ start_wifi_ap.sh
@@ -10,8 +10,9 @@ user=${SUDO_USER:-$(whoami)}
 ip link set dev wlan0 down
 ip address add 192.168.0.1/24 dev wlan0
 cp /home/$user/.digitalpignage/confs/dhcpcd.ap.conf /etc/dhcpcd.conf
+ip link set dev wlan0 up
 service dhcpcd restart
 service dnsmasq restart
-service hostapd restart
-ip link set dev wlan0 up
+service hostapd stop
+service hostapd start
 
