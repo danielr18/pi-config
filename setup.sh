4dc08b00473623474b8bfcf36d9b710e42f8469c
Daniel Reinoso <danielreinoso1807@gmail.com>
2017-12-05 21:57:09 +0000

    First version of setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Works with Debian Stretch
+#
+
+if [ "$EUID" -ne 0 ]
+	then echo "Must be root"
+	exit
+fi
+
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt install nodejs
