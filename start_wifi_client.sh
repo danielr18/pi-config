400557e8f673d9ccbb51158442035c1d42e81b1c
Daniel Reinoso <danielreinoso1807@gmail.com>
2018-01-02 12:46:10 -0300

    Update start_wifi_client.sh

--- start_wifi_client.sh
+++ start_wifi_client.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 if [ "$EUID" -ne 0 ]
-	then echo "Must be root"
-	exit
+        then echo "Must be root"
+        exit
 fi
 
 user=${SUDO_USER:-$(whoami)}
-ip link set dev wlan0 down
 ip address delete 192.168.0.1/24 dev wlan0
+ip link set dev wlan0 down
+service dhcpcd stop
+service hostapd stop
 cp /home/$user/.digitalpignage/confs/dhcpcd.client.conf /etc/dhcpcd.conf
-service dhcpcd restart
-service dnsmasq restart
-service hostapd restart
+service dhcpcd start
 ip link set dev wlan0 up
+
